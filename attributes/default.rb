ca7fa32841200831c5f66b683edcc68c7af4fd47
Rick Mendes <rmendes@intuit.com>
2013-02-27 10:54:47 -0800

    refactored to be intu free

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
-default['intu']['jetty']['version'] = '8.1.7.v20120910'
-default['intu']['jetty']['home']  = '/opt/jetty'
-default['intu']['jetty']['user']  = 'jetty'
-default['intu']['jetty']['user_home']  = '/home/jetty'
-default['intu']['jetty']['group'] = 'jetty'
-default['intu']['jetty']['port']  = 8080
-default['intu']['jetty']['log_dir'] = "#{default['intu']['jetty']['home']}/logs"
+default['jetty']['version'] = '8.1.7.v20120910'
+default['jetty']['home']  = '/opt/jetty'
+default['jetty']['user']  = 'jetty'
+default['jetty']['user_home']  = '/home/jetty'
+default['jetty']['group'] = 'jetty'
+default['jetty']['port']  = 8080
+default['jetty']['log_dir'] = "#{node['jetty']['home']}/logs"
